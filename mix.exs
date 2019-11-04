1a2174b5618e3998334f07833faaf7f741cc6958
Vincent Franco <vince@freshivore.net>
2019-11-04 07:15:11 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule UeberauthEventbrite.Mixfile do
   use Mix.Project
 
-  @version "0.0.3"
+  @version "0.0.4"
   @url "https://github.com/vinniefranco/ueberauth_eventbrite"
 
   def project do
